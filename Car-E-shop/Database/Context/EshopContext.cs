f240e3e90ed1ed490b478c39942d11abdd875c67
Petr Barabáš <petr.barabas@memos.cz>
2023-09-06 11:55:54 +0200

    Added Context  + delete chunk files

---
+++ Car-E-shop/Database/Context/EshopContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Car_E_shop.Database.Context
+{
+    public class EshopContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLExpress;Database=Eshop;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
